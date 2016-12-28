8fb913074bae6c5a1ae08a02d691b0501f0c5800
Mustaf Ahmed <Mustaf Ahmed>
2016-12-27 22:21:27 -0600

    Added option to edit file path of payloads

--- spyder.py
+++ spyder.py
@@ -1,29 +1,29 @@
-
-# List of click commands we need
-	## Command to choose payload file (Save file)
-  ##Command to choose target website (Save website)
-  ## Crawl entire website
-  ## Target specific webpage (it'll find the form)
-
-  ## For crawl and specific page, use a loading thing
 import os
 
+
 def main():
-  # XSS_PAYLOADS - ENV VAR FOR PAYLOAD PATH
-  # Check if this is the first time the user used this program
-	if "XSS_PAYLOADS" not in os.enivron:
+    # First run check
+    if "XSS_PAYLOADS" not in os.enivron:
+        # Prompt the user to enter the payload path
+        XSS_PAYLOADS = input("Enter the file path of your xss vectors")
 
-        # Prompt the user to type in the path
-            XSS_PATH = input("Enter the file path of your xss vectors:")
-        while(len(XSS_PATH) == 0):
-            XSS_Path = input("Enter the file path of your xss vectors:")
+        while (len(XSS_PAYLOADS)) == 0):
+            XSS_PAYLOADS = input("Enter the filepath of your xss vectors")
 
-    # Now set the environment variable XSS_PAYLOADS
-            os.environ["XSS_PAYLOADS"] = XSS_PATH
+        # Now set the environment variable 'XSS_PAYLOADS'
+        os.enivron["XSS_PAYLOADS"] = XSS_PAYLOADS
+    else:
+        # User has used this program before
+        # Checks to see if user would like to edit the filepath of payload
+        editpath = input("Would you like edit the payload path? Y/N ")
+
+        # Edits filepath of payload
+        if editpath == 'Y':
+            editpath = input("Enter the new filepath")
+            os.enivron["XSS_PAYLOADS"] = editpath
+            print("Filepath has been edited to {}!").format(editpath)
         else:
-   	# User has used this program before
 
 
-    if __name__ == '__main__':
-  # run main code here
-    main()
+    if "__name__" == "__main__":
+        main()
