c69b0c901a741816c1ec0e469ee991710d7fcc2a
Mustaf Ahmed <Mustaf Ahmed>
2016-12-27 22:14:13 -0600

    Added option to edit an existing filepath for the xss vectors

--- spyder.py
+++ spyder.py
@@ -13,17 +13,17 @@ def main():
   # Check if this is the first time the user used this program
 	if "XSS_PAYLOADS" not in os.enivron:
 
-    # Prompt the user to type in the path
-    XSS_PATH = input("Enter the file path of your xss vectors:")
-    while(len(XSS_PATH) == 0):
-      XSS_Path = input("Enter the file path of your xss vectors:")
+        # Prompt the user to type in the path
+            XSS_PATH = input("Enter the file path of your xss vectors:")
+        while(len(XSS_PATH) == 0):
+            XSS_Path = input("Enter the file path of your xss vectors:")
 
     # Now set the environment variable XSS_PAYLOADS
-    os.environ["XSS_PAYLOADS"] = XSS_PATH
-  else:
+            os.environ["XSS_PAYLOADS"] = XSS_PATH
+        else:
    	# User has used this program before
 
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
   # run main code here
-  main()
+    main()
