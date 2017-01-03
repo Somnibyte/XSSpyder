57b31f030e28a68f01ecbcf28a3030176e9d9226
Mustaf Ahmed <motrox123@gmail.com>
2017-01-03 15:26:01 -0600

    Added depth of crawling option

--- XSSpyder.py
+++ XSSpyder.py
@@ -1,12 +1,13 @@
-# Crawling Complete
-# Form Testing Incomplete
+# XSS (Cross Site Scripting) Vulnerablity Scanner
 
 import requests
 import unicodedata
 from tld import get_tld
 from bs4 import BeautifulSoup
 
+# Holds all urls that the crawler has found
 visitedurls = []
+# Holds all website paths that the cralwer has found
 branches = []
 
 
@@ -15,38 +16,51 @@ def crawl(seed,maxdepth,currentdepth):
     # defines global variables
     global visitedurls
     global branches
-
+    # Adds a website that has not already been discovered
     if seed not in visitedurls:
         visitedurls.append(seed)
     else:
         return
+    # Increments the current depth of the search
     currentdepth = currentdepth + 1
+    # Sends a get requests for website and returns the html in text form
     data = requests.get(seed).text
+    # Creates a BeautifulSoup Object
     page = BeautifulSoup(data,'html.parser')
+
+    # For each valid link, it will find the links within itself
     for link in page.findAll('a'):
+        # Stores the href value
         l = link.get('href')
+        # Checks if the href is empty
         if l == None:
             continue
+        # Verfies if l stored an href value and whether it has encountred it previously
         if l[0] == "/" and l not in branches:
             branches.append(l)
             l = seed + l
         else:
             continue
+        # Verifies whether l starts with http or htttps
         if l[:4] != 'http' and l[:5] != 'https':
             continue
+        # Verfies whether l's domain matches the seed's domain
         if get_tld(seed) != get_tld(l):
             continue
         visitedurls.append(l)
-
+        # Prevents going over the desired depth of crawling
         if currentdepth < maxdepth:
             crawl(l,maxdepth,currentdepth)
 
 def main():
-
+    # loads payload file with XSS vectors
     payloads = [x for x in open("payloads.txt")]
+    # Counter for maxdepth variable
     currentdepth = 0
+    # Holds the target website
     seed = ""
 
+    # Checks to see if the payloads file is empty
     if len(payloads) == 0:
         print ("There are no XSS vectors within the payloads.txt file")
         return
@@ -57,10 +71,6 @@ def main():
 
         crawl(seed,int(maxdepth),currentdepth)
         print(visitedurls)
-
-
-
-
 
 if __name__ == "__main__":
     main()
